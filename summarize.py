4897fbbb15d7c5c11ebae472be4596991ca93f9c
Jonathon Vogel <jonathon.vogel@gmail.com>
2013-08-03 03:14:38 -0400

    Add doctstrings

--- summarize.py
+++ summarize.py
@@ -11,9 +11,11 @@ LOWER_BOUND = .20 #The low end of shared words to consider
 UPPER_BOUND = .90 #The high end, since anything above this is probably SEO garbage or a duplicate sentence
 
 def is_unimportant(word):
+    """Decides if a word is ok to toss out for the sentence comparisons"""
     return word in ['.', '!', ',', ] or '\'' in word or word in stop_words
 
 def only_important(sent):
+    """Just a little wrapper to filter on is_unimportant"""
     return filter(lambda w: not is_unimportant(w), sent)
 
 def compare_sents(sent1, sent2):
@@ -23,23 +25,30 @@ def compare_sents(sent1, sent2):
     return len(set(only_important(sent1)) & set(only_important(sent2))) / ((len(sent1) + len(sent2)) / 2.0)
 
 def compare_sents_bounded(sent1, sent2):
+    """If the result of compare_sents is not between LOWER_BOUND and
+    UPPER_BOUND, it returns 0 instead, so outliers don't mess with the sum"""
     cmpd = compare_sents(sent1, sent2)
     if cmpd <= LOWER_BOUND or cmpd >= UPPER_BOUND:
         return 0
     return cmpd
 
 def compute_score(sent, sents):
+    """Computes the average score of sent vs the other sentences (the result of
+    sent vs itself isn't counted because it's 1, and that's above
+    UPPER_BOUND)"""
     if not len(sent):
         return 0
     return sum( compare_sents_bounded(sent, sent1) for sent1 in sents ) / float(len(sent))
 
 def summarize_block(block):
+    """Return the sentence that best summarizes block"""
     sents = nltk.sent_tokenize(block)
     word_sents = map(nltk.word_tokenize, sents)
     d = dict( (compute_score(word_sent, word_sents), sent) for sent, word_sent in zip(sents, word_sents) )
     return d[max(d.keys())]
 
 def find_likely_body(b):
+    """Find the tag with the most directly-descended <p> tags"""
     return max(b.find_all(), key=lambda t: len(t.find_all('p', recursive=False)))
 
 def summarize_page(url):
